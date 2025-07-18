c109f6339b7043073f9c94821fb8bd6fa4f56c6f
hariwhois23 <shariprasath.in@gmail.com>
2025-07-18 22:48:55 +0530

    pattern debug

---
+++ patterns/right-triangle.py
@@ -0,0 +1,10 @@
+n = 5 
+pattern = '*'
+whitespace = n-1
+for i in range(1, n+1):
+  for j in  range(whitespace):
+    print('.', end='')
+  whitespace -= 1
+  for k in range(i):
+       print(pattern, end='')
+  print()
